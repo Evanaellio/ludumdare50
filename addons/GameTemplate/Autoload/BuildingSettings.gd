3ad8e6655bbb28d5b79cceb525bf6c13d766d6b8
Foxyhawk <WolfyHawk.Insalan@Gmail.com>
2022-04-03 11:04:57 +0200

    add dam to buildings settings

--- addons/GameTemplate/Autoload/BuildingSettings.gd
+++ addons/GameTemplate/Autoload/BuildingSettings.gd
@@ -3,24 +3,28 @@ extends Node
 enum BuildingID {
 	NONE = -1,
 	CityHall = 0,
-	House = 1,
-	Pump = 2,
+	Dam = 1,
+	House = 2,
+	Pump = 3,
 }
 
 var buildings_max_inhabitants = [
 	5, #CityHall
+	0, #Dam
 	5, #House
 	0, #Pump
 ]
 
 var buildings_max_employees = [
 	0, #CityHall
+	0, #Dam
 	0, #House
 	1, #Pump
 ]
 
 var buildings_cost = [
 	0, #CityHall
+	30, #Dam
 	50, #House
 	25, #Pump
 ]
